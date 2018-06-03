6c8f44f746f92c80c3dd143e7d27189efa5fe88c
stompuri <seppo.tompuri@gmail.com>
2018-06-03 13:07:10 +0300

    Add simple (and ugly) forecast information to ui

--- backend/src/index.js
+++ backend/src/index.js
@@ -19,7 +19,16 @@ const router = new Router();
 app.use(cors());
 
 const fetchWeather = async () => {
+  // api.openweathermap.org/data/2.5/weather?q={city name},{country code}
   const endpoint = `${MAP_URI}/weather?q=${TARGET_CITY}&appid=${APP_ID}&`;
+  const response = await fetch(endpoint);
+
+  return response ? response.json() : {};
+};
+
+const fetchForecast = async () => {
+  // api.openweathermap.org/data/2.5/forecast?q={city name},{country code}
+  const endpoint = `${MAP_URI}/forecast?q=${TARGET_CITY}&appid=${APP_ID}&`;
   const response = await fetch(endpoint);
 
   return response ? response.json() : {};
@@ -27,9 +36,14 @@ const fetchWeather = async () => {
 
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
-
   ctx.type = 'application/json; charset=utf-8';
   ctx.body = weatherData.weather ? weatherData.weather[0] : {};
+});
+
+router.get('/api/forecast', async ctx => {
+  const forecastData = await fetchForecast();
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = forecastData ? forecastData : {};
 });
 
 app.use(router.routes());
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -16,26 +16,57 @@ const getWeatherFromApi = async () => {
   return {};
 };
 
+const getForecastFromApi = async () => {
+  try {
+    const response = await fetch(`${baseURL}/forecast`);
+    return response.json();
+  } catch (error) {
+    console.error(error);
+  }
+
+  return {};
+};
+
 class Weather extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       icon: '',
+      list: [],
     };
   }
 
   async componentWillMount() {
     const weather = await getWeatherFromApi();
-    this.setState({ icon: weather.icon.slice(0, -1) });
+    const forecast = await getForecastFromApi();
+    this.setState({ icon: weather.icon.slice(0, -1), list: forecast.list });
   }
 
   render() {
-    const { icon } = this.state;
-
+    const { icon, list } = this.state;
     return (
-      <div className="icon">
-        { icon && <img src={`/img/${icon}.svg`} alt="icon" /> }
+      <div className="container">
+        <h2>Todays weather ({new Date().toDateString()})</h2>
+        <div className="icon">
+          { icon && <img src={`/img/${icon}.svg`} alt="icon" /> }
+        </div>
+
+        { list.length > 0 &&
+          <div className="forecast">
+            <h2>Forecast</h2>
+            { list.map(day => (
+              <div className="day" key={day.dt}>
+                <div className="icon">
+                  { day.weather[0].icon && <img src={`/img/${day.weather[0].icon.slice(0, -1)}.svg`} alt="icon" /> }
+                </div>
+                <div className="date">
+                  { day.dt_txt.substring(11) }
+                </div>
+              </div>
+            )) }
+          </div>
+        }
       </div>
     );
   }
